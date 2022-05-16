254203426d11b7f675b7c5c4c63dd02276bf7b38
Shegun Babs <shegun.babs@gmail.com>
2022-05-16 10:32:50 +0100

    log viewer install

--- app/Http/Controllers/API/Auth/RegisterController.php
+++ app/Http/Controllers/API/Auth/RegisterController.php
@@ -4,7 +4,21 @@
 namespace App\Http\Controllers\API\Auth;
 
 
+use Illuminate\Http\Request;
+
 class RegisterController
 {
 
+    private array $rules = [
+        'firstname' => ['required', 'string', 'min:3', 'max:20'],
+        'lastname' => ['required', 'string', 'min:3', 'max:20'],
+        'username' => ['required', 'string', 'unique:users'],
+        'email' => ['required', 'email:rfc', 'unique:users'],
+        'password' => ['required', 'string', 'min:8', 'confirmed'],
+    ];
+
+    public function __invoke(Request $request)
+    {
+        $request->validate($this->rules);
+    }
 }
---
+++ database/migrations/2022_05_16_101900_create_wallets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->bigInteger('balance')->default(0);
+            $table->tinyInteger('is_virtual')->default(0);
+            $table->tinyInteger('can_be_negative')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
